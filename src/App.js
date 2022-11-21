52ded34c4d396b6d04ee0700cf25691298571a7d
kleyner villegas <kvillegas@prolesys.com>
2022-11-21 14:01:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect} from "react";
+import {
+	Button,	
+} from "reactstrap";
 
 function App() {
+
+  const [isReadyForInstall, setIsReadyForInstall] = React.useState(false);
+
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // Prevent the mini-infobar from appearing on mobile.
+      event.preventDefault();
+      console.log("👍", "print event", event);
+      // Stash the event so it can be triggered later.
+      window.deferredPrompt = event;
+      // Remove the 'hidden' class from the install button container.
+      setIsReadyForInstall(true);
+    });
+  }, []);
+
+  const upload = async () => {
+		console.log("👍", "butInstall-clicked");
+		const promptEvent = window.deferredPrompt;
+		
+		if (!promptEvent) {
+			// The deferred prompt isn't available.
+			console.log("oops, no prompt event guardado en window");
+			return;
+		}
+		// Show the install prompt.
+		promptEvent.prompt();
+		// Log the result
+		const result = await promptEvent.userChoice;
+		console.log("👍", "userChoice", result);
+		// Reset the deferred prompt variable, since
+		// prompt() can only be called once.
+		window.deferredPrompt = null;
+		// Hide the install button.
+		setIsReadyForInstall(false);
+	}
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +61,11 @@ function App() {
         >
           Learn React
         </a>
+      <Button onClick={upload} className="d-block text-corpoelec mt-2 mb-3">
+        Descaragr App
+      </Button>
       </header>
+
     </div>
   );
 }
